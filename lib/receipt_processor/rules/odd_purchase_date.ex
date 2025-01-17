a65750840db6e109da7181a984ccb197a1600e30
kristoff <kristoffgearhartfinley@gmail.com>
2025-01-16 21:22:29 -0700

    added the odd purchase date handler and test

---
+++ lib/receipt_processor/rules/odd_purchase_date.ex
@@ -0,0 +1,20 @@
+defmodule ReceiptProcessor.Rules.OddPurchaseDate do
+  @name :odd_purchase_date
+
+  def handle(receipt, scoreboard) do
+    result = receipt_handler(receipt)
+    points_awarded = points_handler(result)
+    
+    scoreboard
+    |> ReceiptProcessor.Scoreboard.update(@name, result, points_awarded)
+  end
+
+  defp points_handler(true), do: 6
+  defp points_handler(false), do: 0
+
+  defp receipt_handler(%{purchaseDate: purchase_date}) do 
+    [_year, _month, day] = String.split(purchase_date, "-")
+    {number, _} = Integer.parse(day)
+    rem(number, 2) != 0
+  end
+end
---
+++ test/odd_purchase_date_test.exs
@@ -0,0 +1,23 @@
+defmodule OddPurchaseDateTest do
+  use ExUnit.Case, async: true
+  alias ReceiptProcessor.Scoreboard
+  alias ReceiptProcessor.Rules.OddPurchaseDate
+
+  test "even date" do
+    scoreboard = OddPurchaseDate.handle(
+      %{purchaseDate: "2025-01-16"},
+      Scoreboard.empty()
+    )
+
+    assert {:odd_purchase_date, false, 0} == hd(scoreboard[:rules])
+  end
+
+  test "odd date" do
+    scoreboard = OddPurchaseDate.handle(
+      %{purchaseDate: "2025-01-15"},
+      Scoreboard.empty()
+    )
+
+    assert {:odd_purchase_date, true, 6} == hd(scoreboard[:rules])
+  end
+end
