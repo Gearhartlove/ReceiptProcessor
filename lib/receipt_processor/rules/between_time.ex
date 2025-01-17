a805a7e77207f4c16e4bf706aee2b79f51dc6ae3
kristoff <kristoffgearhartfinley@gmail.com>
2025-01-16 21:32:10 -0700

    add the between time handler and tests

---
+++ lib/receipt_processor/rules/between_time.ex
@@ -0,0 +1,20 @@
+defmodule ReceiptProcessor.Rules.BetweenTime do
+  @name :between_time
+
+  def handle(receipt, scoreboard) do
+    result = receipt_handler(receipt)
+    points_awarded = points_handler(result)
+    
+    scoreboard
+    |> ReceiptProcessor.Scoreboard.update(@name, result, points_awarded)
+  end
+
+  defp points_handler(true), do: 10
+  defp points_handler(false), do: 0
+
+  defp receipt_handler(%{purchaseTime: purchase_time}) do 
+    [hour, seconds] = String.split(purchase_time, ":")
+    {seconds, _} = Integer.parse(seconds)
+    hour in ["14", "15"] and seconds != 0
+  end
+end
