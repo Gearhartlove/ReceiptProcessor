722578e5315ca1098ea65e8edc342ff117f02f27
kristoff <kristoffgearhartfinley@gmail.com>
2025-01-17 17:14:46 -0700

    add accurate error responses according to api.yml

--- lib/receipt_processor/controller.ex
+++ lib/receipt_processor/controller.ex
@@ -6,9 +6,10 @@ defmodule ReceiptProcessor.Controller do
   `process` takes a connection's receipt, scores it, generates a unique id, and saves the 
   score and the receipt along side the id for future lookup.
   """
-  def process(conn) do
-    # TODO add a try around this and return a 404 specified by api.yml if request is bad
-    receipt = struct(Receipt, conn.body_params)
+
+  def process({:error, :request, conn}), do: send_resp(conn, 404, "The receipt is invalid.")
+
+  def process({conn, receipt}) do
     scoreboard = ReceiptProcessor.Rules.score(receipt)
     id = :erlang.unique_integer() |> to_string()
     :ok = ReceiptProcessor.Storage.save(id, {receipt, scoreboard})
@@ -20,20 +21,36 @@ defmodule ReceiptProcessor.Controller do
     send_resp(conn, 200, response)
   end
 
+  def process(conn) do
+    try do
+      receipt = struct!(Receipt, conn.body_params)
+      process({conn, receipt})
+    rescue
+      ArgumentError -> process({:error, :request, conn})
+    end
+  end
+
   @doc """
   `points` takes a connection's id and looks up the total for that id. It then returns this
   total to the client.
   """
-  def points(conn) do
-    id = conn.params["id"]
-    IO.puts("Getting params for id. id=[#{id}]")
-    # TODO if no id is found, then return a 404 NotFonud error, ref api.yml for this
-    {_, %{total: total}} = ReceiptProcessor.Storage.get(id)
+  def points({:error, :missing, conn}), do: send_resp(conn, 404, "No receipt found for that ID.")
 
+  def points({conn, total}) do
     response =
       %{points: total}
       |> JSON.encode!()
 
     send_resp(conn, 200, response)
+  end
+
+  def points(conn) do
+    id = conn.params["id"]
+    case ReceiptProcessor.Storage.get(id) do
+      {_, %{total: total}} -> 
+        points({conn, total})
+      _ -> 
+        points({:error, :missing, conn})
+    end
   end
 end
